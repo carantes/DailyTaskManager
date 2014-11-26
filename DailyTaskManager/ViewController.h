0ef81d6097c1d70b7d56cb84e8d51f9939121dc3
Carlos Eduardo Arantes Ferreira <Dudu@Carloss-MacBook.local>
2014-11-26 00:12:59 -0200

    Initial Commit

---
+++ DailyTaskManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DailyTaskManager
+//
+//  Created by Carlos Eduardo Arantes Ferreira on 26/11/14.
+//  Copyright (c) 2014 Mobistart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DailyTaskManager/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DailyTaskManager
+//
+//  Created by Carlos Eduardo Arantes Ferreira on 26/11/14.
+//  Copyright (c) 2014 Mobistart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
